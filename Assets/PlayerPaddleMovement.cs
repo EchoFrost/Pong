9b521d0c7a9c5f1e4ba0427637bdc7ae83452b44
EchoFrost <whitesellbt@gmail.com>
2018-08-23 17:40:48 -0700

    Added paddle sprite, game object, physics components, began movement script

---
+++ Assets/PlayerPaddleMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddleMovement : MonoBehaviour {
+
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode movedown = KeyCode.S;
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
