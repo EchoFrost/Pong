c4ce319c41481defac31fa31b216281c4aa417cf
EchoFrost <whitesellbt@gmail.com>
2018-08-27 00:02:20 -0700

    Added some basic ball control

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class BallControl : MonoBehaviour {
 
+	private Rigidbody2D rb2d;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb2d = GetComponent<Rigidbody2D>();
+		Invoke("GoBall", 2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void GoBall(){
+		float rand = Random.Range(0, 2);
+		if(rand < 1){
+			rb2d.AddForce(new Vector2(20, -15));
+		} else {
+			rb2d.AddForce(new Vector2(-20, -15));
+		}
+	}
+
+	void ResetBall(){
+		rb2d.velocity = Vector2.zero;
+		transform.position = Vector2.zero;
+	}
+
+	void RestartGame(){
+		ResetBall();
+		Invoke("GoBall", 1);
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		if(coll.collider.CompareTag("Player")){
+			Vector2 vel;
+			vel.x = rb2d.velocity.x;
+			vel.y = (rb2d.velocity.y / 2) + (coll.collider.attachedRigidbody.velocity.y / 3);
+			rb2d.velocity = vel;
+		}
 	}
 }
